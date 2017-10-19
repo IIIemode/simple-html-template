5794d1cc83f72aab66cd989b7bcfa75179bb5b6d
Denis Zhibenkov <deniszhibenkov@gmail.com>
2017-10-19 23:26:46 +0300

    change classes in script.js on correct

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,50 +1,43 @@
 (() => {
 
     const d = document;
-    const tshirt = d.querySelectorAll('.sections-rightSection-category'); 
+    const tshirt = d.querySelectorAll('.right-section__category'); 
     const smallTshirt = tshirt[0].querySelector('#smallTshirt');
     const middleTshirt = tshirt[0].querySelector('#middleTshirt');
     const largeTshirt = tshirt[0].querySelector('#largeTshirt');
     const whiteTshirt = tshirt[1].querySelector('#whiteTshirt');
     const yellowTshirt = tshirt[1].querySelector('#yellowTshirt');
     const greenTshirt = tshirt[1].querySelector('#greenTshirt');
-    const pictureTshirt = d.querySelectorAll('.sections-leftSection img')[0];
-
-    // const rightSection = d.querySelectorAll('.sections-rightSection')[0];
-    // console.log(rightSection.offsetHeight);
-    // console.log(pictureTshirt.offsetHeight);
-    // pictureTshirt.style.marginTop = -(rightSection.offsetHeight - pictureTshirt.offsetHeight) / 3 + 'px';
-    // console.log(pictureTshirt.style.marginTop);
-    // console.log(pictureTshirt.offsetWidth);
+    const pictureTshirt = d.querySelectorAll('.left-section__img')[0];
 
     whiteTshirt.addEventListener('click', ( {target} ) => {
         pictureTshirt.src = 'img/tshirts/tshirt_white.jpg';
         for (var i = 0; i < tshirt[1].children.length; i++) {
-            if (tshirt[1].children[i].classList.contains('sections-rightSection_activeColor')) {
-                tshirt[1].children[i].classList.remove('sections-rightSection_activeColor');
+            if (tshirt[1].children[i].classList.contains('right-section__color_active')) {
+                tshirt[1].children[i].classList.remove('right-section__color_active');
             }        
         }
-        target.classList.add('sections-rightSection_activeColor');      
+        target.classList.add('right-section__color_active');      
     });
 
     yellowTshirt.addEventListener('click', ( {target} ) => {
         pictureTshirt.src = 'img/tshirts/tshirt_yellow.jpg';
         for (var i = 0; i < tshirt[1].children.length; i++) {
-            if (tshirt[1].children[i].classList.contains('sections-rightSection_activeColor')) {
-                tshirt[1].children[i].classList.remove('sections-rightSection_activeColor');
+            if (tshirt[1].children[i].classList.contains('right-section__color_active')) {
+                tshirt[1].children[i].classList.remove('right-section__color_active');
             }        
         }
-        target.classList.add('sections-rightSection_activeColor');      
+        target.classList.add('right-section__color_active');      
     });
 
     greenTshirt.addEventListener('click', ( {target} ) => {
         pictureTshirt.src = 'img/tshirts/tshirt_green.jpg';
         for (var i = 0; i < tshirt[1].children.length; i++) {
-            if (tshirt[1].children[i].classList.contains('sections-rightSection_activeColor')) {
-                tshirt[1].children[i].classList.remove('sections-rightSection_activeColor');
+            if (tshirt[1].children[i].classList.contains('right-section__color_active')) {
+                tshirt[1].children[i].classList.remove('right-section__color_active');
             }        
         }
-        target.classList.add('sections-rightSection_activeColor');      
+        target.classList.add('right-section__color_active');      
     });
 
 })();
