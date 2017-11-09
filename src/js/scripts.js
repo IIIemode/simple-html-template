061cf421d5bd8d0a91ae2dba9139dbc5f9c4bee4
IIIemo <deniszhibenkov@gmail.com>
2017-11-09 19:48:57 +0300

    Add tag 'picture' and compression jpg images in gimp_2

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,11 +3,15 @@
     const d = document;
     const tShirt = d.getElementById('choiceForm');
     const leftSectionImg = d.querySelectorAll('.product-image__img')[0];
+    const leftSectionImgTablet = d.querySelectorAll('.product-image__tablet-img')[0];
 
     tShirt.addEventListener('click', ( {target} ) => {
         if (target.name === 'color') {
             const colors = target.parentNode.children;
-            leftSectionImg.src = 'img/tshirts/tshirt_' + target.dataset.color + '.jpg';
+
+            leftSectionImgTablet.srcset = 'img/tshirts/tshirt_' + target.dataset.color + '.jpg';
+            leftSectionImg.src = leftSectionImgTablet.srcset;
+              
             for (let i = 0; i < colors.length; i++) {
                 if (colors[i].classList.contains('product-info__color_active')) {
                     colors[i].classList.remove('product-info__color_active');
