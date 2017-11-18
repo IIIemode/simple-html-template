84930fde047740420934f218ebe76bcfbc8f5f30
IIIemo <deniszhibenkov@gmail.com>
2017-11-18 18:31:14 +0300

    Add size and color to localStorage

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,32 +3,36 @@ import PropertySelector from './property-selector.js';
 const d = document;
 const Dispatcher = d.getElementById('choiceForm');
 
+loadPage();
+
 new PropertySelector(d.getElementById('colorList'));
 new PropertySelector(d.getElementById('sizeList'));
 
 Dispatcher.addEventListener('property-selected', ev => {
     const data = ev.detail;
 
+    if (data.type === 'size') {
+        changePrice();
+        showCurrentBorder(data.type, data.value);
+        setToLocalStorage(data.type, data.value);
+    }
+
     if (data.type === 'color') {
         changePicture(data.value);
         changePrice();
         showCurrentBorder(data.type, data.value);
-    }
-
-    if (data.type === 'size') {
-        changePrice();
-        showCurrentBorder(data.type, data.value);
+        setToLocalStorage(data.type, data.value);
     }
 });
-
-function changePrice() {
-    document.getElementById('priceVal').innerHTML = +new Date() % 10000 + '₽';
-}
 
 function changePicture(color) {
     const pathToTshirt = 'img/tshirts/tshirt_';
     d.getElementById('productPicture__tablet-img').srcset = pathToTshirt + color + '.jpg';
     d.getElementById('productPicture__img').src = pathToTshirt + color + '.jpg';
+}
+
+function changePrice() {
+    document.getElementById('priceVal').innerHTML = +new Date() % 10000 + '₽';
 }
 
 function showCurrentBorder(type, value) {
@@ -44,4 +48,22 @@ function showCurrentBorder(type, value) {
     }
 
     d.querySelectorAll(classNameItem)[0].classList.add(classNameItemActive);
+}
+
+function setToLocalStorage(type, value) {
+    localStorage.setItem(type, value);
+}
+
+function loadPage() {
+    const color = localStorage.getItem('color');
+    const size = localStorage.getItem('size');
+
+    if (size) {
+        showCurrentBorder('size', size);
+    }
+
+    if (color) {
+        changePicture(color);
+        showCurrentBorder('color', color);
+    }
 }
